25281047a97608d2e3139dd5dfeb2a68c3a03f72
Behnaz Pourafshari <behnaz.pourafshari@storyshareplatform.com>
2020-06-24 15:38:46 +0100

    get logged in user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,30 @@
 const express = require('express');
-
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
 
-//@route GET api/profile
-//@desc Test route
+//@route GET api/profile/me
+//@desc Profile route
 //@access private
-
-router.get('/', (req, res, next) => {
-  res.send('profile route');
+router.get('/me', auth, async (req, res, next) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate({
+      path: 'user',
+      select: ' name avatar'
+    });
+    if (!profile) {
+      return res.status(400).json({
+        msg: 'There is no profile for this user'
+      });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
