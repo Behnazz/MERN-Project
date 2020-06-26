cbb8fa5f926fbec898f67a31c7fcccd1c1c71f97
Behnaz Pourafshari <behnaz.pourafshari@storyshareplatform.com>
2020-06-26 16:24:27 +0100

    Post model

---
+++ models/Post.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const { model } = require('./User');
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: { type: mongoose.Schema.ObjectId, ref: 'users' }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('post', PostSchema);
