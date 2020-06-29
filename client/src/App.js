b7ed0f2db772b16a2a74f55e857d1688f10d9a11
Behnaz Pourafshari <behnaz.pourafshari@storyshareplatform.com>
2020-06-29 17:18:03 +0100

    Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from '../src/Components/layout/Navbar';
+import Landing from '../src/Components/layout/Landing';
+import Register from '../src/Components/Auth/Register';
+import Login from '../src/Components/Auth/Login';
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <h1>App</h1>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/Register' component={Register} />
+          <Route exact path='/Login' component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
