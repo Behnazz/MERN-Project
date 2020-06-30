a822d13682fd82cd117677e668737ec1d746e0a7
Behnaz Pourafshari <behnaz.pourafshari@storyshareplatform.com>
2020-06-30 10:46:02 +0100

    login and registration form is set

--- client/src/Components/Auth/Register.js
+++ client/src/Components/Auth/Register.js
@@ -1,7 +1,94 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+  //handle input change
+  const handleInput = e =>
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+
+  //handle submit form
+  const handleSubmit = async e => {
+    e.preventDefault();
+    //make sure passwords are the same
+    if (password !== password2) {
+      console.log('Passwords do not mach');
+    } else {
+      console.log('Success');
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Create Your Account
+      </p>
+      <form className='form' onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            value={name}
+            onChange={handleInput}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={handleInput}
+            required
+          />
+          <small className='form-text'>
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={handleInput}
+            minLength='6'
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='password2'
+            value={password2}
+            onChange={handleInput}
+            minLength='6'
+            required
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Register' />
+      </form>
+      <p className='my-1'>
+        Already have an account?
+        <Link to='/login'>Sign In</Link>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Register;
